9c46d5dcd66afad30b610c87b929086e435b5b64
novin <temka.mondrunov@mail.ru>
2024-12-26 15:48:33 +0300

    ясделалэто

--- main.py
+++ main.py
@@ -13,11 +13,11 @@ def start(message):
 def get_weather(message):
     city = message.text.strip().lower()
     res = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API}&units=metric')
-    if res.statud_code == 200:
+    if res.status_code == 200:
         data = json.loads(res.text)
         temp = data["main"]["temp"]
         bot.reply_to(message, f'Погода сейчас: {data["main"]["temp"]}')
-        image = 'sunny.png' if temp > 5.0 else 'sun.png'
+        image = 'sun.png' if temp > 5.0 else 'sunny.png'
         file = open('./' + image, 'rb')
         bot.send_photo(message.chat.id, file)
     else:
